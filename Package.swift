19b9fc6f4e1f37820af3f052f11ec4f7ee1cd144
Gustavo Perdomo <gperdomor@gmail.com>
2017-06-13 16:45:10 -0400

    remove example code

--- Package.swift
+++ Package.swift
@@ -2,10 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyBeaverProvider",
-    targets: [
-        Target(name: "AppExample", dependencies: ["SwiftyBeaverProvider"]),
-        Target(name: "RunExample", dependencies: ["AppExample"])
-    ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1)
