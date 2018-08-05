6f0a9b2b44dd444a19232ae982b4995ca906c901
Nathan Harris <nathan.harris@globant.com>
2018-08-05 13:08:49 -0700

    Update SwiftyBeaver package version to 1.6

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.5.2")
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "1.6")
     ],
     targets: [
         .target(name: "SwiftyBeaverProvider", dependencies: ["SwiftyBeaver", "Vapor"]),
