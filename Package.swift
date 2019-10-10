4fe41ba8ea593aa2dcdf4ce25058ec438d1191ce
John Heaton <jheaton.dev@gmail.com>
2019-10-10 14:04:35 -0400

    Add macOS 10.12 minimum supported platform version
    This fixes the current build failure due to the dependency 'SwiftyBeaver' also having
    this same requirement. This produces the following error message without the change:
    `error: the product 'SwiftyBeaver' requires minimum platform version 10.12 for macos platform`

--- Package.swift
+++ Package.swift
@@ -1,8 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "SwiftyBeaverProvider",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         .library(name: "SwiftyBeaverProvider", targets: ["SwiftyBeaverProvider"])
     ],
